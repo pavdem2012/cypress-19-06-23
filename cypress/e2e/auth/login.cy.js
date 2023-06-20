bad22106a6f68ef2757698fd33abb9a95b1b0a32
Pavdem2012 <pavdem2012@gmail.com>
2023-06-20 14:10:36 +0700

    add cypress structure and some tests

---
+++ cypress/e2e/auth/login.cy.js
@@ -0,0 +1,81 @@
+describe('Авторизация',() =>{
+    beforeEach(()=>{
+        cy.visit('https://try.vikunja.io/login')
+        cy.contains('Войти')
+    })
+
+    it('Нельзя авторизоваться без пароля',()=>{
+        cy.get('#username').type('demo')
+        cy.get('button').contains('Войти').click()
+        cy.get('.help').contains('Введите пароль')
+        cy.url().should('eq', 'https://try.vikunja.io/login')
+
+    })
+
+    it('Нельзя авторизоваться без имени пользователя',()=>{
+        cy.get('#password').type('demo')
+        cy.get('button').contains('Войти').click()
+        cy.get('.help').contains('Введите имя пользователя')
+        cy.url().should('eq', 'https://try.vikunja.io/login')
+
+    })
+
+    it('Успешная авторизация',()=>{
+        cy.get('#username').type('demo')
+        cy.get('#password').type('demo')
+        cy.get('button').contains('Войти').click({force:true})
+        cy.url().should('eq', 'https://try.vikunja.io/')
+
+    })
+
+    it('Успешный разлогин',()=>{
+        cy.get('#username').type('demo')
+        cy.get('#password').type('demo')
+        cy.get('.button').click()
+        cy.url().should('eq', 'https://try.vikunja.io/')
+        cy.get('.username').click({force:true})
+        cy.get(':nth-child(4) > span').click({force:true})
+        cy.url().should('eq', 'https://try.vikunja.io/login')
+    })
+
+    it('Вывод модального окна "О Vikunja"',()=>{
+        cy.get('#username').type('demo')
+        cy.get('#password').type('demo')
+        cy.get('.button').click()
+        cy.url().should('eq', 'https://try.vikunja.io/')
+        cy.get('.username').click({force:true})
+        cy.get('[href="/about"] > span').click({force:true})
+        cy.url().should('eq', 'https://try.vikunja.io/about')
+        cy.get('.card-header-title').should('contain',"О Vikunja")
+        cy.get('.p-4')
+            .should('contain', 'Версия фронтенда:')
+            .should('contain', 'Версия API:');
+    })
+
+    it('Вывод модального окна "Сочетания клавиш"',()=>{
+        cy.get('#username').type('demo')
+        cy.get('#password').type('demo')
+        cy.get('.button').click()
+        cy.url().should('eq', 'https://try.vikunja.io/')
+        cy.get('.username').click({force:true})
+        cy.get('.dropdown-content > :nth-child(2) > span').click()
+        cy.url().should('eq', 'https://try.vikunja.io/')
+        cy.get('.card-header-title').should('contain',"Сочетания клавиш")
+        cy.get('.card-content > .content > :nth-child(1)').should('contain',"Основное")
+        cy.get('.card-content > .content > :nth-child(3)').should('contain',"Навигация")
+        cy.get('.content > :nth-child(5)').should('contain',"Канбан")
+        cy.get('.content > :nth-child(8)').should('contain',"Просмотр проекта")
+        cy.get('.content > :nth-child(11)').should('contain',"Страница задачи")
+    })
+
+    it('Переход к странице настроек',()=>{
+        cy.get('#username').type('demo')
+        cy.get('#password').type('demo')
+        cy.get('.button').click()
+        cy.url().should('eq', 'https://try.vikunja.io/')
+        cy.get('.username').click({force:true})
+        cy.get('[href="/user/settings"] > span').click()
+        cy.url().should('eq', 'https://try.vikunja.io/user/settings/general')
+        cy.get('.card-header-title').should('contain', "Основные настройки")
+    })
+})
